3ca08f7cee5b240cf4f511c8de2826c80281f523
De'vID <De.vID.jonpIn@gmail.com>
2013-01-16 18:18:34 +0100

    add script to generate db from xml directly using sqlite3 commands

--- KlingonAssistant/data/reconstruct.sh
+++ KlingonAssistant/data/reconstruct.sh
@@ -1,2 +1,31 @@
 #!/bin/sh
+
+# Concatenate data into one file.
 cat mem-00-header.xml mem-01-b.xml mem-02-ch.xml mem-03-D.xml mem-04-gh.xml mem-05-H.xml mem-06-j.xml mem-07-l.xml mem-08-m.xml mem-09-n.xml mem-10-ng.xml mem-11-p.xml mem-12-q.xml mem-13-Q.xml mem-14-r.xml mem-15-S.xml mem-16-t.xml mem-17-tlh.xml mem-18-v.xml mem-19-w.xml mem-20-y.xml mem-21-a.xml mem-22-e.xml mem-23-I.xml mem-24-o.xml mem-25-u.xml mem-26-footer.xml > mem.xml
+
+# Convert from xml to sql instructions.
+./xml2sql.pl > mem.sql
+
+# Print any entries with duplicate columns.
+grep "ARRAY" mem.sql
+
+# Create db binary.
+if [ -f ../assets/qawHaq.db ];
+then
+    mv ../assets/qawHaq.db ../assets/qawHaq.db~
+fi
+sqlite3 ../assets/qawHaq.db < mem.sql
+
+# Sanity check.
+sqlite3 ../assets/qawHaq.db .dump > sanity.sql
+diff mem.sql sanity.sql
+
+# Pause (in case of error).
+read -n1 -r -p "Press any key to continue..." key
+echo
+
+# Clean up temporary files.
+rm mem.xml
+rm mem.sql
+rm sanity.sql
+
---
+++ KlingonAssistant/data/xml2sql.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl
+
+# use modules
+use File::Slurp;
+use XML::Simple;
+
+# output Unicode
+binmode(STDOUT, ":utf8");
+
+# create xml object
+$xml = new XML::Simple;
+
+# read xml file
+$sm_export = read_file('mem.xml');
+
+# do substitutions to make it ready for conversion
+$sm_export =~ s/\s*<!--.*?-->//sg;
+$sm_export =~ s/<table name="mem">(.*?)<\/table>/<mem>\1<\/mem>/sg;
+$sm_export =~ s/<column name="(.*?)">(.*?)<\/column>/<\1>\2<\/\1>/sg;
+$sm_export =~ s/'/''/g;
+
+# convert processed xml file to xml
+$data = $xml->XMLin($sm_export, suppressempty => '');
+
+# print sql file header
+print "PRAGMA foreign_keys=OFF;\n".
+      "BEGIN TRANSACTION;\n".
+      "CREATE TABLE \"android_metadata\" (\"locale\" TEXT DEFAULT 'en_US');\n".
+      "INSERT INTO \"android_metadata\" VALUES('en_US');\n".
+      "CREATE TABLE \"mem\" (\"_id\" INTEGER PRIMARY KEY ,\"entry_name\" TEXT,\"part_of_speech\" TEXT,\"definition\" TEXT,\"synonyms\" TEXT,\"antonyms\" TEXT,\"see_also\" TEXT,\"notes\" TEXT,\"hidden_notes\" TEXT,\"components\" TEXT,\"examples\" TEXT,\"search_tags\" TEXT,\"source\" TEXT, \"definition_de\" TEXT DEFAULT \"\");\n";
+
+# cycle through and print the entries
+foreach $e (@{$data->{database}->{mem}})
+{
+    print "INSERT INTO \"mem\" VALUES(";
+    print $e->{_id}, ",'";
+    print $e->{entry_name}, "','";
+    print $e->{part_of_speech}, "','";
+    print $e->{definition}, "','";
+    print $e->{synonyms}, "','";
+    print $e->{antonyms}, "','";
+    print $e->{see_also}, "','";
+    print $e->{notes}, "','";
+    print $e->{hidden_notes}, "','";
+    print $e->{components}, "','";
+    print $e->{examples}, "','";
+    print $e->{search_tags}, "','";
+    print $e->{source}, "','";
+    print $e->{definition_de}, "');\n";
+}
+
+# print sql file footer
+print "COMMIT;\n";
