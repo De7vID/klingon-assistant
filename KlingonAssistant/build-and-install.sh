c0c2ae5fc226beaa240fa0c167500b4260702f05
De'vID <De.vID.jonpIn@gmail.com>
2013-01-31 20:04:24 +0100

    pause in build script before pushing to device

--- KlingonAssistant/build-and-install.sh
+++ KlingonAssistant/build-and-install.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/bin/bash
+
+# Generate the database.
 cd data
 ./generate_db.sh
 cd ..
+
+# Build the debug build.
 ant debug
 if [ $? -eq 0 ]
 then
+    # Pause before pushing.
+    read -n1 -r -p "Press any key to push to device..."
+    echo
+
+    # Push to device.
     adb install -r bin/KlingonAssistant-debug.apk
 else
     echo ERROR: Failed to compile.
