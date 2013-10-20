0cfe410b18379215db72cda019c3f035056ffc72
De'vID <De.vID.jonpIn@gmail.com>
2013-10-20 17:00:49 +0200

    added simple floating window

---
+++ KlingonAssistant/src/org/tlhInganHol/android/klingonassistant/FloatingWindow.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2013 De'vID jonpIn (David Yonge-Mallo)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tlhInganHol.android.klingonassistant;
+
+import wei.mark.standout.StandOutWindow;
+import wei.mark.standout.constants.StandOutFlags;
+import wei.mark.standout.ui.Window;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+
+public class FloatingWindow extends StandOutWindow {
+
+	@Override
+	public String getAppName() {
+		return "FloatingWindow";
+	}
+
+	@Override
+	public int getAppIcon() {
+		return android.R.drawable.ic_menu_close_clear_cancel;
+	}
+
+	@Override
+	public void createAndAttachView(int id, FrameLayout frame) {
+		// create a new layout from body.xml
+		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.simple, frame, true);
+	}
+
+	// the window will be centered
+	@Override
+	public StandOutLayoutParams getParams(int id, Window window) {
+		return new StandOutLayoutParams(id, 250, 300,
+				StandOutLayoutParams.CENTER, StandOutLayoutParams.CENTER);
+	}
+
+	// move the window by dragging the view
+	@Override
+	public int getFlags(int id) {
+		return super.getFlags(id) | StandOutFlags.FLAG_BODY_MOVE_ENABLE
+				| StandOutFlags.FLAG_WINDOW_FOCUSABLE_DISABLE;
+	}
+
+	@Override
+	public String getPersistentNotificationMessage(int id) {
+		return "Click to close the FloatingWindow";
+	}
+
+	@Override
+	public Intent getPersistentNotificationIntent(int id) {
+		return StandOutWindow.getCloseIntent(this, FloatingWindow.class, id);
+	}
+}
