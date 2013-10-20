77e09b4ca5b5d150aaf2bffa1921b3e84ad86879
De'vID <De.vID.jonpIn@gmail.com>
2013-10-20 17:46:52 +0200

    changed from simple to multiwindow

--- KlingonAssistant/src/org/tlhInganHol/android/klingonassistant/FloatingWindow.java
+++ KlingonAssistant/src/org/tlhInganHol/android/klingonassistant/FloatingWindow.java
@@ -16,13 +16,38 @@
 
 package org.tlhInganHol.android.klingonassistant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import wei.mark.standout.StandOutWindow;
 import wei.mark.standout.constants.StandOutFlags;
 import wei.mark.standout.ui.Window;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+/**
+ * This implementation provides multiple windows. You may extend this class or
+ * use it as a reference for a basic foundation for your own windows.
+ * 
+ * <p>
+ * Functionality includes system window decorators, moveable, resizeable,
+ * hideable, closeable, and bring-to-frontable.
+ * 
+ * <p>
+ * The persistent notification creates new windows. The hidden notifications
+ * restores previously hidden windows.
+ * 
+ * @author Mark Wei <markwei@gmail.com>
+ * 
+ */
 public class FloatingWindow extends StandOutWindow {
 
 	@Override
@@ -32,37 +57,125 @@ public class FloatingWindow extends StandOutWindow {
 
 	@Override
 	public int getAppIcon() {
-		return android.R.drawable.ic_menu_close_clear_cancel;
+		return android.R.drawable.ic_menu_add;
+	}
+
+	@Override
+	public String getTitle(int id) {
+		return getAppName() + " " + id;
 	}
 
 	@Override
 	public void createAndAttachView(int id, FrameLayout frame) {
 		// create a new layout from body.xml
 		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		inflater.inflate(R.layout.simple, frame, true);
+		View view = inflater.inflate(R.layout.floating, frame, true);
+
+		TextView idText = (TextView) view.findViewById(R.id.id1);
+		idText.setText(String.valueOf(id));
 	}
 
-	// the window will be centered
+	// every window is initially same size
 	@Override
 	public StandOutLayoutParams getParams(int id, Window window) {
-		return new StandOutLayoutParams(id, 250, 300,
-				StandOutLayoutParams.CENTER, StandOutLayoutParams.CENTER);
+		return new StandOutLayoutParams(id, 400, 300,
+				StandOutLayoutParams.AUTO_POSITION,
+				StandOutLayoutParams.AUTO_POSITION, 100, 100);
 	}
 
-	// move the window by dragging the view
+	// we want the system window decorations, we want to drag the body, we want
+	// the ability to hide windows, and we want to tap the window to bring to
+	// front
 	@Override
 	public int getFlags(int id) {
-		return super.getFlags(id) | StandOutFlags.FLAG_BODY_MOVE_ENABLE
-				| StandOutFlags.FLAG_WINDOW_FOCUSABLE_DISABLE;
+		return StandOutFlags.FLAG_DECORATION_SYSTEM
+				| StandOutFlags.FLAG_BODY_MOVE_ENABLE
+				| StandOutFlags.FLAG_WINDOW_HIDE_ENABLE
+				| StandOutFlags.FLAG_WINDOW_BRING_TO_FRONT_ON_TAP
+				| StandOutFlags.FLAG_WINDOW_EDGE_LIMITS_ENABLE
+				| StandOutFlags.FLAG_WINDOW_PINCH_RESIZE_ENABLE;
+	}
+
+	@Override
+	public String getPersistentNotificationTitle(int id) {
+		return getAppName() + " Running";
 	}
 
 	@Override
 	public String getPersistentNotificationMessage(int id) {
-		return "Click to close the FloatingWindow";
+		return "Click to add a new " + getAppName();
+	}
+
+	// return an Intent that creates a new FloatingWindow
+	@Override
+	public Intent getPersistentNotificationIntent(int id) {
+		return StandOutWindow.getShowIntent(this, getClass(), getUniqueId());
 	}
 
 	@Override
-	public Intent getPersistentNotificationIntent(int id) {
-		return StandOutWindow.getCloseIntent(this, FloatingWindow.class, id);
+	public int getHiddenIcon() {
+		return android.R.drawable.ic_menu_info_details;
+	}
+
+	@Override
+	public String getHiddenNotificationTitle(int id) {
+		return getAppName() + " Hidden";
+	}
+
+	@Override
+	public String getHiddenNotificationMessage(int id) {
+		return "Click to restore #" + id;
+	}
+
+	// return an Intent that restores the FloatingWindow
+	@Override
+	public Intent getHiddenNotificationIntent(int id) {
+		return StandOutWindow.getShowIntent(this, getClass(), id);
+	}
+
+	@Override
+	public Animation getShowAnimation(int id) {
+		if (isExistingId(id)) {
+			// restore
+			return AnimationUtils.loadAnimation(this,
+					android.R.anim.slide_in_left);
+		} else {
+			// show
+			return super.getShowAnimation(id);
+		}
+	}
+
+	@Override
+	public Animation getHideAnimation(int id) {
+		return AnimationUtils.loadAnimation(this,
+				android.R.anim.slide_out_right);
+	}
+
+	@Override
+	public List<DropDownListItem> getDropDownItems(int id) {
+		List<DropDownListItem> items = new ArrayList<DropDownListItem>();
+		items.add(new DropDownListItem(android.R.drawable.ic_menu_help,
+				"About", new Runnable() {
+
+					@Override
+					public void run() {
+						Toast.makeText(
+								FloatingWindow.this,
+								getAppName()
+										+ " is a demonstration of StandOut.",
+								Toast.LENGTH_SHORT).show();
+					}
+				}));
+		items.add(new DropDownListItem(android.R.drawable.ic_menu_preferences,
+				"Settings", new Runnable() {
+
+					@Override
+					public void run() {
+						Toast.makeText(FloatingWindow.this,
+								"There are no settings.", Toast.LENGTH_SHORT)
+								.show();
+					}
+				}));
+		return items;
 	}
 }
