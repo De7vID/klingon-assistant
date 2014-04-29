bb9c6fd4f61a4b146afe55a85f436b6f20f5ce67
De'vID <De.vID.jonpIn@gmail.com>
2014-04-29 07:13:11 +0200

    2014-04-29

--- KlingonAssistant/v.sh
+++ KlingonAssistant/v.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-vim -p AndroidManifest.xml data/VERSION src/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java CHANGELOG res/values/strings.xml
+vim -p AndroidManifest.xml data/VERSION src/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java CHANGELOG res/values/strings.xml res/values-de/strings.xml
