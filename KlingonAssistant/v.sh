092d7789f723a237ef81d2610759822398c342a7
De'vID <De.vID.jonpIn@gmail.com>
2014-09-27 11:33:00 +0200

    update script to generate xml file before checking audio files

--- KlingonAssistant/v.sh
+++ KlingonAssistant/v.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 vim -p AndroidManifest.xml data/VERSION src/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java CHANGELOG res/values/strings.xml res/values-de/strings.xml
+# ./write_db.sh
