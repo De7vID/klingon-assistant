86547da2faa9efc3138599475ad42d5dc9686a7c
De'vID <De.vID.jonpIn@gmail.com>
2014-12-02 17:24:24 +0100

    version 2014.12.02a

--- KlingonAssistant/src/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java
+++ KlingonAssistant/src/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java
@@ -92,7 +92,7 @@ public class KlingonContentDatabase {
 
   // This should be kept in sync with the version number in the database
   // entry {boQwI':n}.
-  private static final int                     DATABASE_VERSION             = 201411300;
+  private static final int                     DATABASE_VERSION             = 201412020;
 
   private final KlingonDatabaseOpenHelper      mDatabaseOpenHelper;
   private static final HashMap<String, String> mColumnMap                   = buildColumnMap();
