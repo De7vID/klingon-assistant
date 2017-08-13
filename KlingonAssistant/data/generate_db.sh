8e96b3f78d9cdf0efaf47121e464f43faf470271
De'vID <De.vID.jonpIn@gmail.com>
2017-08-13 21:23:27 +0000

    move non-canon stuff to a separate file

--- KlingonAssistant/data/generate_db.sh
+++ KlingonAssistant/data/generate_db.sh
@@ -15,7 +15,7 @@ then
 fi
 
 # Concatenate data into one xml file.
-cat mem-00-header.xml mem-01-b.xml mem-02-ch.xml mem-03-D.xml mem-04-gh.xml mem-05-H.xml mem-06-j.xml mem-07-l.xml mem-08-m.xml mem-09-n.xml mem-10-ng.xml mem-11-p.xml mem-12-q.xml mem-13-Q.xml mem-14-r.xml mem-15-S.xml mem-16-t.xml mem-17-tlh.xml mem-18-v.xml mem-19-w.xml mem-20-y.xml mem-21-a.xml mem-22-e.xml mem-23-I.xml mem-24-o.xml mem-25-u.xml mem-26-footer.xml > mem.xml
+cat mem-00-header.xml mem-01-b.xml mem-02-ch.xml mem-03-D.xml mem-04-gh.xml mem-05-H.xml mem-06-j.xml mem-07-l.xml mem-08-m.xml mem-09-n.xml mem-10-ng.xml mem-11-p.xml mem-12-q.xml mem-13-Q.xml mem-14-r.xml mem-15-S.xml mem-16-t.xml mem-17-tlh.xml mem-18-v.xml mem-19-w.xml mem-20-y.xml mem-21-a.xml mem-22-e.xml mem-23-I.xml mem-24-o.xml mem-25-u.xml mem-26-footer.xml mem-27-non-canon.xml > mem.xml
 
 if [[ $XMLONLY ]]
 then
