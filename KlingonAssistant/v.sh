145fa42118ba21f137980556acdc3daeddd38229
De'vID <De.vID.jonpIn@gmail.com>
2017-09-08 20:20:45 +0000

    renumber database before writing; write database whenever version number changed

--- KlingonAssistant/v.sh
+++ KlingonAssistant/v.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 vim -p AndroidManifest.xml data/VERSION src/org/tlhInganHol/android/klingonassistant/KlingonContentDatabase.java CHANGELOG
 # vim -p res/values/strings.xml res/values-de/strings.xml res/values-es/strings.xml
-# ./write_db.sh
+./write_db.sh
