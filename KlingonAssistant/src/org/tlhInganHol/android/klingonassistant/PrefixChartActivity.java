3e47e48cbc08e0f6acdec096848704af85300bb5
De'vID <De.vID.jonpIn@gmail.com>
2017-09-18 14:06:38 +0200

    add back accidentally deleted files

---
+++ KlingonAssistant/src/org/tlhInganHol/android/klingonassistant/PrefixChartActivity.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2014 De'vID jonpIn (David Yonge-Mallo)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.tlhInganHol.android.klingonassistant;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+/** Displays the prefix chart. */
+public class PrefixChartActivity extends BaseActivity {
+  // private static final String TAG = "PrefixChartActivity";
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+
+    if (sharedPrefs.getBoolean(
+        Preferences.KEY_KLINGON_UI_CHECKBOX_PREFERENCE, /* default */ false)) {
+      setDrawerContentView(R.layout.prefix_charts_tlh);
+    } else {
+      setDrawerContentView(R.layout.prefix_charts);
+    }
+  }
+
+  @Override
+  public void onBackPressed() {
+    super.onBackPressed();
+    overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+  }
+}
