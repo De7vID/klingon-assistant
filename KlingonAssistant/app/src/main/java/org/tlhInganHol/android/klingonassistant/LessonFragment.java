81cfeeb97ab20daac50a649db5cf511ef803450c
De'vID <De.vID.jonpIn@gmail.com>
2017-10-08 04:48:04 +0000

    add RadioButtons

--- KlingonAssistant/app/src/main/java/org/tlhInganHol/android/klingonassistant/LessonFragment.java
+++ KlingonAssistant/app/src/main/java/org/tlhInganHol/android/klingonassistant/LessonFragment.java
@@ -16,17 +16,10 @@
 package org.tlhInganHol.android.klingonassistant;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.database.Cursor;
-import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.text.Html;
 import android.text.SpannableStringBuilder;
 import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
@@ -34,10 +27,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -110,17 +99,15 @@ public class LessonFragment extends EntryFragment {
     // Set up possible additional views.
     mChoicesGroup = (RadioGroup) rootView.findViewById(R.id.choices);
     if (mEntries != null) {
-      // TODO: use entries
-      Cursor cursor = getActivity().managedQuery(
-                  Uri.parse(KlingonContentProvider.CONTENT_URI + "/lookup"),
-                  null /* all columns */,
-                  null,
-                  new String[] {"bIjatlhHa'chugh qaHoH."},
-                  // new String[] {"Qong, Sop@@Qong:v, Sop:v"},
-                  null);
-      // MultipleChoiceAdapter multipleChoiceAdapter = new MultipleChoiceAdapter(cursor);
-      // mListView.setAdapter(multipleChoiceAdapter);
-      // mListView.setOnItemClickListener(multipleChoiceAdapter);
+      for (int i = 0; i < mEntries.size(); i++) {
+        // TODO: Include entry definition, format text.
+        // TODO: No linking.
+        RadioButton choiceButton = new RadioButton(getActivity());
+        SpannableStringBuilder choiceText = new SpannableStringBuilder(mEntries.get(i));
+        processMixedText(choiceText, mEntries.get(i), null);
+        choiceButton.setText(choiceText);
+        mChoicesGroup.addView(choiceButton);
+      }
       mChoicesGroup.setVisibility(View.VISIBLE);
       mChoicesGroup.invalidate();
       // TODO: Fix selector font size, colours.
@@ -141,127 +128,132 @@ public class LessonFragment extends EntryFragment {
   }
 
   // List adapter for word selection and multiple-choice quizzes.
-  class MultipleChoiceAdapter extends BaseAdapter implements AdapterView.OnItemClickListener {
+  // class MultipleChoiceAdapter extends BaseAdapter implements AdapterView.OnItemClickListener {
 
-    private final Cursor mCursor;
-    private final LayoutInflater mInflater;
-    private int mSelectedPosition = -1;
-    private RadioButton mSelectedButton = null;
+  //   private final Cursor mCursor;
+  //   private final LayoutInflater mInflater;
+  //   private int mSelectedPosition = -1;
+  //   private RadioButton mSelectedButton = null;
 
-    public MultipleChoiceAdapter(Cursor cursor) {
-      mCursor = cursor;
-      mInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    }
+  //   public MultipleChoiceAdapter(Cursor cursor) {
+  //     mCursor = cursor;
+  //     mInflater = (LayoutInflater)
+  // getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+  //   }
 
-    @Override
-    public int getCount() {
-      return mCursor.getCount();
-    }
+  //   @Override
+  //   public int getCount() {
+  //     return mCursor.getCount();
+  //   }
 
-    @Override
-    public Object getItem(int position) {
-      return position;
-    }
+  //   @Override
+  //   public Object getItem(int position) {
+  //     return position;
+  //   }
 
-    @Override
-    public long getItemId(int position) {
-      return position;
-    }
+  //   @Override
+  //   public long getItemId(int position) {
+  //     return position;
+  //   }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-      LinearLayout view = (convertView != null) ? (LinearLayout) convertView : createView(parent);
+  //   @Override
+  //   public View getView(int position, View convertView, ViewGroup parent) {
+  //     LinearLayout view = (convertView != null) ? (LinearLayout) convertView :
+  // createView(parent);
 
-      // Have to set the check state here because the views are recycled.
-      RadioButton button = (RadioButton) view.findViewById(R.id.radio);
-      if (position == mSelectedPosition) {
-        button.setChecked(true);
-      } else {
-        button.setChecked(false);
-      }
+  //     // Have to set the check state here because the views are recycled.
+  //     RadioButton button = (RadioButton) view.findViewById(R.id.radio);
+  //     if (position == mSelectedPosition) {
+  //       button.setChecked(true);
+  //     } else {
+  //       button.setChecked(false);
+  //     }
 
-      mCursor.moveToPosition(position);
-      bindView(view, mCursor);
-      return view;
-    }
+  //     mCursor.moveToPosition(position);
+  //     bindView(view, mCursor);
+  //     return view;
+  //   }
 
-    private LinearLayout createView(ViewGroup parent) {
-      // Use a modified version of android.R.simple_list_item_2_single_choice
-      // which has been adapted for our needs.
-      LinearLayout item =
-          (LinearLayout)
-              mInflater.inflate(R.layout.simple_list_item_2_single_choice, parent, false);
-      return item;
-    }
+  //   private LinearLayout createView(ViewGroup parent) {
+  //     // Use a modified version of android.R.simple_list_item_2_single_choice
+  //     // which has been adapted for our needs.
+  //     LinearLayout item =
+  //         (LinearLayout)
+  //             mInflater.inflate(R.layout.simple_list_item_2_single_choice, parent, false);
+  //     return item;
+  //   }
 
-    private void bindView(LinearLayout view, Cursor cursor) {
-      KlingonContentProvider.Entry entry = new KlingonContentProvider.Entry(cursor, getActivity());
+  //   private void bindView(LinearLayout view, Cursor cursor) {
+  //     KlingonContentProvider.Entry entry = new KlingonContentProvider.Entry(cursor,
+  // getActivity());
 
-      // Note that we override the typeface and text size here, instead of in
-      // the xml, because putting it there would also change the appearance of
-      // the Preferences page. We fully indent suffixes, but only half-indent verbs.
-      String indent1 =
-          entry.isIndented() ? (entry.isVerb() ? "&nbsp;&nbsp;" : "&nbsp;&nbsp;&nbsp;&nbsp;") : "";
-      String indent2 =
-          entry.isIndented()
-              ? (entry.isVerb()
-                  ? "&nbsp;&nbsp;&nbsp;&nbsp;"
-                  : "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
-              : "";
+  //     // Note that we override the typeface and text size here, instead of in
+  //     // the xml, because putting it there would also change the appearance of
+  //     // the Preferences page. We fully indent suffixes, but only half-indent verbs.
+  //     String indent1 =
+  //         entry.isIndented() ? (entry.isVerb() ? "&nbsp;&nbsp;" : "&nbsp;&nbsp;&nbsp;&nbsp;") :
+  // "";
+  //     String indent2 =
+  //         entry.isIndented()
+  //             ? (entry.isVerb()
+  //                 ? "&nbsp;&nbsp;&nbsp;&nbsp;"
+  //                 : "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;")
+  //             : "";
 
-      TextView text1 = view.findViewById(android.R.id.text1);
-      TextView text2 = view.findViewById(android.R.id.text2);
-      SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
-      if (!sharedPrefs.getBoolean(
-          Preferences.KEY_KLINGON_FONT_CHECKBOX_PREFERENCE, /* default */ false)) {
-        // Use serif for the entry, so capital-I and lowercase-l are distinguishable.
-        text1.setTypeface(Typeface.SERIF);
-        text1.setText(Html.fromHtml(indent1 + entry.getFormattedEntryName(/* isHtml */ true)));
-      } else {
-        // Preference is set to display this in {pIqaD}!
-        text1.setTypeface(KlingonAssistant.getKlingonFontTypeface(getActivity()));
-        text1.setText(Html.fromHtml(indent1 + entry.getEntryNameInKlingonFont()));
-      }
-      text1.setTextSize(22);
+  //     TextView text1 = view.findViewById(android.R.id.text1);
+  //     TextView text2 = view.findViewById(android.R.id.text2);
+  //     SharedPreferences sharedPrefs =
+  // PreferenceManager.getDefaultSharedPreferences(getActivity());
+  //     if (!sharedPrefs.getBoolean(
+  //         Preferences.KEY_KLINGON_FONT_CHECKBOX_PREFERENCE, /* default */ false)) {
+  //       // Use serif for the entry, so capital-I and lowercase-l are distinguishable.
+  //       text1.setTypeface(Typeface.SERIF);
+  //       text1.setText(Html.fromHtml(indent1 + entry.getFormattedEntryName(/* isHtml */ true)));
+  //     } else {
+  //       // Preference is set to display this in {pIqaD}!
+  //       text1.setTypeface(KlingonAssistant.getKlingonFontTypeface(getActivity()));
+  //       text1.setText(Html.fromHtml(indent1 + entry.getEntryNameInKlingonFont()));
+  //     }
+  //     text1.setTextSize(22);
 
-      // TODO: Colour attached affixes differently from verb.
-      text1.setTextColor(entry.getTextColor());
+  //     // TODO: Colour attached affixes differently from verb.
+  //     text1.setTextColor(entry.getTextColor());
 
-      // Use sans serif for the definition.
-      text2.setTypeface(Typeface.SANS_SERIF);
-      text2.setText(Html.fromHtml(indent2 + entry.getFormattedDefinition(/* isHtml */ true)));
-      text2.setTextSize(14);
-      text2.setTextColor(0xFFC0C0C0);
-    }
+  //     // Use sans serif for the definition.
+  //     text2.setTypeface(Typeface.SANS_SERIF);
+  //     text2.setText(Html.fromHtml(indent2 + entry.getFormattedDefinition(/* isHtml */ true)));
+  //     text2.setTextSize(14);
+  //     text2.setTextColor(0xFFC0C0C0);
+  //   }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-      RadioButton button = (RadioButton) view.findViewById(R.id.radio);
-      if (position != mSelectedPosition && mSelectedButton != null) {
-        mSelectedButton.setChecked(false);
-      }
-      button.setChecked(true);
-      mSelectedPosition = position;
-      mSelectedButton = button;
+  //   @Override
+  //   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+  //     RadioButton button = (RadioButton) view.findViewById(R.id.radio);
+  //     if (position != mSelectedPosition && mSelectedButton != null) {
+  //       mSelectedButton.setChecked(false);
+  //     }
+  //     button.setChecked(true);
+  //     mSelectedPosition = position;
+  //     mSelectedButton = button;
 
-      if (getCount() == 1) {
-        // Launch entry the regular way, as there's only one result.
-        mCursor.moveToPosition(position);
-        // launchEntry(mCursor.getString(KlingonContentDatabase.COLUMN_ID));
-      } else {
-        // There's a list of results, so launch a list of entries. Instead of passing in
-        // one ID, we pass in a comma-separated list. We also append the position of the
-        // selected entry to the end.
-        StringBuilder entryList = new StringBuilder();
-        for (int i = 0; i < getCount(); i++) {
-          mCursor.moveToPosition(i);
-          entryList.append(mCursor.getString(KlingonContentDatabase.COLUMN_ID));
-          entryList.append(",");
-        }
-        entryList.append(position);
-        mCursor.moveToPosition(position);
-        // launchEntry(entryList.toString());
-      }
-    }
-  }
+  //     if (getCount() == 1) {
+  //       // Launch entry the regular way, as there's only one result.
+  //       mCursor.moveToPosition(position);
+  //       // launchEntry(mCursor.getString(KlingonContentDatabase.COLUMN_ID));
+  //     } else {
+  //       // There's a list of results, so launch a list of entries. Instead of passing in
+  //       // one ID, we pass in a comma-separated list. We also append the position of the
+  //       // selected entry to the end.
+  //       StringBuilder entryList = new StringBuilder();
+  //       for (int i = 0; i < getCount(); i++) {
+  //         mCursor.moveToPosition(i);
+  //         entryList.append(mCursor.getString(KlingonContentDatabase.COLUMN_ID));
+  //         entryList.append(",");
+  //       }
+  //       entryList.append(position);
+  //       mCursor.moveToPosition(position);
+  //       // launchEntry(entryList.toString());
+  //     }
+  //   }
+  // }
 }
