c05264f6fa46ce92261f6ba4e2de677c6a096f72
De'vID <De.vID.jonpIn@gmail.com>
2017-10-23 15:51:52 +0000

    uncomment ForegroundColorSpan fix

--- KlingonAssistant/app/src/main/java/org/tlhInganHol/android/klingonassistant/EntryFragment.java
+++ KlingonAssistant/app/src/main/java/org/tlhInganHol/android/klingonassistant/EntryFragment.java
@@ -480,17 +480,12 @@ public class EntryFragment extends Fragment {
       // There is a bug in Android 6 (API 23) and 7 (API 24 and 25) which
       // messes up the sort order of the ForegroundColorSpans.
       // See: https://github.com/De7vID/klingon-assistant/issues/190
-      // It used to be possible to work around this by removing existing
-      // ForegroundColorSpans first, but this is broken on the Google Pixel
-      // devices (and possibly others) because the user's highlighting is
-      // implemented using a ForegroundColorSpan. Removing this span causes
-      // a call to setSpan(-1, -1) which causes an IndexOutOfBoundsException.
-      // The work-around also does not work when running on Chromebook (version 
-      // 61.0.3163.120). Commenting out the work-around.
-      // ForegroundColorSpan[] oldSpans = ssb.getSpans(m.start(), end, ForegroundColorSpan.class);
-      // for (ForegroundColorSpan span : oldSpans) {
-      //   ssb.removeSpan(span);
-      // }
+      // The work-around does not work when running on Chromebook (version
+      // 61.0.3163.120).
+      ForegroundColorSpan[] oldSpans = ssb.getSpans(m.start(), end, ForegroundColorSpan.class);
+      for (ForegroundColorSpan span : oldSpans) {
+        ssb.removeSpan(span);
+      }
       ssb.setSpan(new ForegroundColorSpan(linkedEntry.getTextColor()), m.start(), end, FINAL_FLAGS);
       String linkedPos = linkedEntry.getBracketedPartOfSpeech(/* isHtml */ false);
       if (!linkedPos.equals("") && linkedPos.length() > 1) {
